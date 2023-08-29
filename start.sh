f080170a6d7576ffe0859646f1f7ef805436ad9e
[roi]ghteously_ <68617740+roighteously@users.noreply.github.com>
2023-08-29 21:51:30 +0000

    asdf

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
-apt-get -y update 
-apt-get -y install nitrogen firefox xdg-user-dirs
+sudo apt-get -y update 
+sudo apt-get -y install nitrogen firefox xdg-user-dirs
 xdg-user-dirs-update
 cd ~
 wget https://m.media-amazon.com/images/I/71nJtF2woeL._AC_UY1000_.jpg -O bg.jpg
